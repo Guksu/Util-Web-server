a6a2b48c0647d73ab69b7e5e0a6ed834355d3c98
Guksu <yung3152@daum.net>
2021-12-26 21:13:27 +0900

    fassion query 수정

--- src/fassion/fassion.controller.ts
+++ src/fassion/fassion.controller.ts
@@ -9,11 +9,36 @@ export class FassionController {
   async getFassionImg(@Req() req: Request) {
     const ID_KEY = process.env.NAVER_CLIENT_KEY;
     const SECRET_KEY = process.env.NAVER_CLIENT_SECRET;
+    const temp: any = req.query.isTemp;
+    let style: string;
+    {
+      parseInt(temp) <= 4 && style === `패딩`;
+    }
+    {
+      parseInt(temp) <= 8 && parseInt(temp) > 4 && style === `코트, 니트`;
+    }
+    {
+      parseInt(temp) <= 11 && parseInt(temp) > 8 && style === `니트, 자켓`;
+    }
+    {
+      parseInt(temp) <= 16 && parseInt(temp) > 11 && style === `가디건, 야상`;
+    }
+    {
+      parseInt(temp) <= 19 && parseInt(temp) > 16 && style === `맨투맨, 가디건`;
+    }
+    {
+      parseInt(temp) <= 22 &&
+        parseInt(temp) > 19 &&
+        style === `긴팔 , 얇은 가디건`;
+    }
+    {
+      parseInt(temp) <= 27 && parseInt(temp) > 22 && style === `반팔 , 반바지`;
+    }
     try {
       const { data: searchData } = await axios.get(
         'https://openapi.naver.com/v1/search/image',
         {
-          params: { query: `${req.query.isTemp}도 ${req.query.gender} 옷차림` },
+          params: { query: ` ${req.query.gender} ${style} 코디` },
           headers: {
             'X-Naver-Client-Id': `${ID_KEY}`,
             'X-Naver-Client-Secret': `${SECRET_KEY}`,
